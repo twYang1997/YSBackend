17f3ed81e84167043789949931ce7808eefb7e10
timmy <timmy860930@gmail.com>
2023-09-04 18:41:52 +0800

    init config
    sql server config, jpa config

--- src/main/java/com/yessir/backend/DemoApplication.java
+++ src/main/java/com/yessir/backend/DemoApplication.java
@@ -1,30 +1,14 @@
 package com.yessir.backend;
 
-import java.util.stream.Stream;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.yessir.backend.model.User;
-import com.yessir.backend.repository.UserRepository;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-    CommandLineRunner init(UserRepository userRepository) {
-        return args -> {
-        	Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel", "Jenny").forEach(name -> {
-                User user = new User(name, name.toLowerCase() + "@domain.com");
-                userRepository.save(user);
-            });
-            userRepository.findAll().forEach(System.out::println);
-        };
-    }
 }

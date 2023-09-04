7c3db62e15691976cad938d6c599574e8e48ed7e
timmy <timmy860930@gmail.com>
2023-09-04 11:41:36 +0800

    test

--- src/main/java/com/yessir/backend/DemoApplication.java
+++ src/main/java/com/yessir/backend/DemoApplication.java
@@ -20,7 +20,7 @@ public class DemoApplication {
 	@Bean
     CommandLineRunner init(UserRepository userRepository) {
         return args -> {
-        	Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+        	Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel", "Jenny").forEach(name -> {
                 User user = new User(name, name.toLowerCase() + "@domain.com");
                 userRepository.save(user);
             });
